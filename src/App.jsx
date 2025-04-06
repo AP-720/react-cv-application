a39ae445f0dae5a4979bfc0ab1c771a1d6b2686b
Andy Price <andyprice720@gmail.com>
2025-04-06 22:01:30 +0100

    Initial commit and CSS set up
    - set up css file structure and imports.
    - think I have over engineered it, good to practice this methodology.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import "./css/main.css";
+
+function App() {
+	return (
+		<div className="d-flex justify-content-center flex-column align-items-center">
+			<h1>Hello</h1>
+			<button className="button">Click</button>
+		</div>
+	);
+}
+
+export default App;
